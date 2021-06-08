8b6afdd181476b6cb9f8d7e187735057e40ed430
Sandeep Shrivastava <shrivastavasandeep13@gmail.com>
2021-06-08 21:47:46 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,81 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const request=require("request");
+const https=require("https");
+
+const app=express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+
+app.get("/", function(req, res)
+{
+  res.sendFile(__dirname+"/signup.html");
+})
+
+app.post("/",function(req,res)
+{
+
+  const firstName=req.body.fname;
+  const lastName=req.body.lname;
+  const email=req.body.email;
+  const data={
+    members:[
+      {
+      email_address:email,
+      status:"subscribed",
+      merge_fields : {
+        FNAME:firstName,
+        LNAME:lastName
+      }
+    }
+  ]
+};
+
+const jsonData=JSON.stringify(data);
+
+const url="https://us6.api.mailchimp.com/3.0/lists/12f8776e29";
+const options={
+  method:"POST",
+  auth:"Sandeep:950e385a49388ab972dd0fde4a011c94-us6"
+}
+
+const request=https.request(url,options,function(response)
+{
+  if(response.statusCode===200)
+  {
+    res.sendFile(__dirname+"/success.html");
+  }
+  else{
+    res.sendFile(__dirname+"/failure.html");
+  }
+  response.on("data",function(data)
+{
+  console.log(JSON.parse(data));
+})
+
+});
+
+
+
+request.write(jsonData);
+request.end();
+
+});
+
+app.post("/failure",function(req,res){
+  res.redirect("/");
+});
+
+
+
+app.listen(3000, function()
+{
+  console.log("Server is running om Port 3000");
+})
+
+
+//950e385a49388ab972dd0fde4a011c94-us6
+//12f8776e29
